706dc5188810af1250b1d11f01f931f59fc998e0
Jes Bak Hansen <jeshansen@gmail.com>
2019-05-26 22:16:38 +0200

    Module 9: Create Simple Add Course Form (and binding)

--- src/components/courses/CoursesPage.tsx
+++ src/components/courses/CoursesPage.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import {
+  ICoursesPageProps,
+  ICoursesPageState
+} from "../interfaces/CoursesPageInterfaces";
 
-class CoursesPage extends React.Component {
+type TextChangedEvent = React.ChangeEvent<HTMLInputElement>;
+
+class CoursesPage extends React.Component<
+  ICoursesPageProps,
+  ICoursesPageState
+> {
+  state: ICoursesPageState = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = (event: TextChangedEvent) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
---
+++ src/components/interfaces/CoursesPageInterfaces.ts
@@ -0,0 +1,7 @@
+export interface ICoursesPageProps {}
+export interface ICourse {
+  title: string;
+}
+export interface ICoursesPageState {
+  course: ICourse;
+}
