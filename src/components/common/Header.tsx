0991d988cadbbc8055f96ffb3947d8b8c60d8509
Jes Bak Hansen <jeshansen@gmail.com>
2019-05-26 21:41:02 +0200

    Module 5: Create Header

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const AboutPage: React.FC = () => {
+  const activeStyle = { color: "#F15B2A" };
+
+  return (
+    <div>
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+    </div>
+  );
+};
+
+export default AboutPage;
