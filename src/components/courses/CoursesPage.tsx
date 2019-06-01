cd744aaed095001bd5e2c98e7f80944e8cf5bec6
Jes Bak Hansen <jeshansen@gmail.com>
2019-06-01 15:28:51 +0200

    Redux Flow : Handle Submit

--- src/components/courses/CoursesPage.tsx
+++ src/components/courses/CoursesPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { CoursePageState, CoursePageProps, TextChangedEvent } from "../../types";
+import { RootState, CoursePageProps, TextChangedEvent, SubmitEvent } from "../../types";
 
-class CoursesPage extends React.Component<CoursePageProps, CoursePageState> {
+class CoursesPage extends React.Component<CoursePageProps, RootState> {
   constructor(props: CoursePageProps) {
     super(props);
 
@@ -12,9 +12,15 @@ class CoursesPage extends React.Component<CoursePageProps, CoursePageState> {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
   };
+
+  handleSubmit = (event: SubmitEvent) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add course</h3>
         <input type="text" onChange={this.handleChange} value={this.state.course.title} />
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
-export type CoursePageState = { course: { title: string } };
+export type RootState = { course: { title: string } };
 export type CoursePageProps = { course: { title: string } };
 export type TextChangedEvent = React.ChangeEvent<HTMLInputElement>;
+export type SubmitEvent = React.FormEvent<HTMLFormElement>;
